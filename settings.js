ff7ce080da1d2b638fa1e568075cc26143bbfe58
ryanzhengker11 <ryanzhengker02@gmail.com>
2023-02-03 23:42:01 +0700

    tes

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ require('dotenv').config()
 
 //_______________________ ┏  Main Setting  ┓ _______________________\\
 
-creator = '乂𝘼𝙡𝙞𝙥乂' // Nama 
+creator = 'RyanzOfc' // Nama 
 port = 8080 //port host
-LimitApikey = 200 // Limit Apikey default
+LimitApikey = 1000 // Limit Apikey default
 
 //___________ ┏  Database Mongodb Setting  ┓ ___________\\
 
@@ -30,8 +30,8 @@ keymongodb = process.env.mongodb //example "mongodb+srv://user:password@name_dat
 //___________ ┏  SMTP Setting  ┓ ___________\\
 
 //note service SMTP Gmail Tidak Boleh Send Kat Temp Email Kalau mahu pakai temp email buat di service lain
-usetempemail = false // kalau true boleh sing up pakai email temp kalau pakai service gmail pakai false je 
-servicesmtp = 'Gmail'//Ada gmail ada sendinblue ikut masing2 limit gmail 500 limit sendinblue 300 free. List service smtp : https://nodemailer.com/smtp/well-known/
+usetempemail = true // kalau true boleh sing up pakai email temp kalau pakai service gmail pakai false je 
+servicesmtp = 'Zoho'//Ada gmail ada sendinblue ikut masing2 limit gmail 500 limit sendinblue 300 free. List service smtp : https://nodemailer.com/smtp/well-known/
 sendemail = process.env.sendemail //Email SMTP
 fromsendemail = process.env.fromsendemail // From Email SMTP *kalau takde custom email kena sama email dengan Email SMTP
 sendpwmail = process.env.sendpwmail // Password Email SMTP
